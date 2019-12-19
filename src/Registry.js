2d23a8c54257681cb8b8ddb81b9bff5d271ef8e0
mlee138 <mlee138@binghamton.edu>
2019-12-19 18:45:48 -0500

    updated registry

--- src/Registry.js
+++ src/Registry.js
@@ -4,8 +4,11 @@ import './stylesheets/Registry.css'
 function Registry() {
     return(
         <div id="registry">
+            <img src="./images/piggy-bank-with-dollar-coins.png"/>
+            <div>Icon made by FreePik from www.flaticon.com</div>
             <p>Honestly, please just give us some good cold hard CA<span>$</span>H.</p>
             <p>We won't accept checks, but we will accept Amazon gift cards.</p>
+            
         </div>
     );
 }
